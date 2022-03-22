22e3e9172a24a431cd7c5438c75be12ba26e623d
verninp <paul.vernin@grenoble-inp.org>
2022-03-22 16:50:36 +0100

    mult_native

--- tp1/fct_mult_native.s
+++ tp1/fct_mult_native.s
@@ -10,17 +10,17 @@ uint32_t mult_native(void)
     .globl mult_native
 /* DEBUT DU CONTEXTE
 fonction :
-     nom_de_fonction  : feuille ou non feuille
+     mult_native  : feuille
 contexte :
-     parametre_0      : registre a0
-     parametre_1      : registre ai; pile *(sp+n)
-     variable_locale0 : registre t0
-     variable_locale1 : pile *(sp+k)
-     ra               : pile *(sp+p)
-     variable_globale : memoire [section nom_de_section]
+     x : mémoire
+     y : mémoire
  */
 mult_native:
+    lw t0, x /* t0=x */
+    lw t1, y /* t1=y */
+    lui t2, 0 /* res */
 mult_native_fin_prologue:
-/* A compléter */
+    mul t2, t0, t1
+    mv a0, t2
 mult_native_debut_epilogue:
     ret
