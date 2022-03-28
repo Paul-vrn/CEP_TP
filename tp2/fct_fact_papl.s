5d0f01789a9779aae0f989ce6243e6ee74ec035b
verninp <paul.vernin@grenoble-inp.org>
2022-03-28 19:45:21 +0200

    fact papl fixed

--- tp2/fct_fact_papl.s
+++ tp2/fct_fact_papl.s
@@ -18,14 +18,14 @@ uint32_t fact_papl(uint32_t n)
   Fonction :
      fact_papl : non feuille
   Contexte :
-     ra : pile *(sp+0)
-     n : registre a0; pile *(sp+4)
-     tmp :/ registre a0,a1
- */
+     ra : pile *(sp+12)
+     n : registre a0; pile *(sp+0)
+     tmp : pile *(sp+4)/*(sp+8)
+*/
 fact_papl:
-    addi sp, sp, -8
-    sw ra, 8(sp)
-    sw a0, 4(sp) /* bit de point faible */
+    addi sp, sp, -16
+    sw ra, 12(sp)
+    sw a0, 0(sp) /* bit de point faible */
 fact_papl_fin_prologue:
     mv t0, a0 /* t0=n*/
     sltiu t2, a0, 2 /* t2=n<2*/
@@ -36,16 +36,18 @@ fact_papl_fin_prologue:
     else:
         addi a0, a0, -1 /*n-1*/
         jal fact_papl
-        lw t0, 4(sp) /*t0 = fact(n-1)*/
-        mul a0, t0, a0 /* bits de point faible */
-        mulhu a1, t0, a0 /* bits de point fort*/
-        slt t3, zero, a1 /* t3=0<a1*/
+        lw t0, 0(sp) /*t0 = fact(n-1)*/
+        mv t1, a0
+        mul a0, t0, t1 /* bits de point faible */
+        mulhu a1, t0, t1 /* bits de point fort*/
+        sw a0, 4(sp)
+        sw a1, 8(sp)
+        sltu t3, zero, a1 /* t3=0<a1*/
         beqz t3, skip_error
         mv a0, t0
         jal erreur_fact
         skip_error:
 fact_papl_debut_epilogue:
-    lw ra, 8(sp)
-    addi sp, sp, 8
+    lw ra, 12(sp)
+    addi sp, sp, 16
     ret
-
