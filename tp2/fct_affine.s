a2d2cec225952a3c193a5815a7aecfb8ef3aaeb2
verninp <paul.vernin@grenoble-inp.org>
2022-03-28 11:18:55 +0200

    qsd

--- tp2/fct_affine.s
+++ tp2/fct_affine.s
@@ -11,24 +11,24 @@ uint32_t affine(uint32_t a, uint32_t b, uint32_t x)
   Fonction :
      affine : non feuille
   Contexte :
+     a : registre a0; pile *(sp+0)
+     b : registre a1; pile *(sp+4)
+     x : registre a2; pile *(sp+8)
      ra : pile *(sp+16)
-     a : registre a0; pile *(sp+4)
-     b : registre a1; pile *(sp+8)
-     x : registre a2; pile *(sp+12)
  */
 affine:
     addi sp, sp, -16
-    sw a0, 4(sp)
-    sw a1, 8(sp)
-    sw a2, 12(sp)
-    sw ra, 16(sp)
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw ra, 12(sp)
 affine_fin_prologue:
-    lw a0, 12(sp)
-    lw a1, 4(sp)
+    mv a0, a2
+    lw a1, 0(sp)
     jal mult
-    lw t1, 8(sp)
+    lw a1, 4(sp)
     add a0, a0, t1
 affine_debut_epilogue:
-    lw ra, 16(sp)
+    lw ra, 12(sp)
     addi sp, sp, 16
     ret
