6368c02b50e2a93b1e9e5155637bd34d7b490479
verninp <paul.vernin@grenoble-inp.org>
2022-04-06 18:53:21 +0200

    palin fix

--- tp3/fct_palin.s
+++ tp3/fct_palin.s
@@ -16,23 +16,28 @@ bool palin(const char *ch)
     /* bool palin(char *ch) */
 /* DEBUT DU CONTEXTE
   Fonction :
-     palin  : feuille
+     palin  : non feuille
   Contexte :
-     ch : registre a0
-     inf : registre t1
-     sup : registre t2
+     ch : registre a0; pile *(sp+0) (champ de type char*)
+     inf : registre t1; pile *(sp+4) (champ de type uint32_t)
+     sup : registre t2; pile *(sp+8) (champ de type uint32_t)
+     ra : pile *(sp+16)
  */
 palin:
-    addi sp, sp, -8
-    sw ra, 4(sp)
+    addi sp, sp, -16
+    sw ra, 12(sp)
     sw a0, 0(sp)
 palin_fin_prologue:
     lui t1, 0 /* inf = 0*/
+    sw t1, 4(sp)
     jal strlen
     mv t2, a0
     addi t2, t2, -1 /* sup = strlen(ch) - 1*/
+    sw t2, 8(sp)
     lw a0, 0(sp)
 while:
+    lw t1, 4(sp)
+    lw t2, 8(sp)
     slt t3, t1, t2 /* t3 <= t1 < t2 */
     add t4, a0, t1 /* t4 = adresse ch[inf] */
     lbu t4, 0(t4) /* t4 = ch[inf]*/
@@ -42,18 +47,14 @@ while:
     beqz t3, fin_while
         addi t1, t1, 1 /* inf++ */
         addi t2, t2, -1 /* sup-- */
+        sw t1, 4(sp)
+        sw t2, 8(sp)
     j while
 fin_while:
     slt t5, t1, t2
     addi t4, x0, 1
     sub a0, t4, t5
-/*    bge t1, t2, cbon
-pasbon:
-    addi a0, x0, 0
-    j palin_debut_epilogue
-cbon:
-    addi a0, x0, 1*/
 palin_debut_epilogue:
-    lw ra, 4(sp)
-    addi sp, sp, 8
+    lw ra, 12(sp)
+    addi sp, sp, 16
     ret
