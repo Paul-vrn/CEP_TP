1b263aadc33b883e880e8a62f03bcd81678209e4
verninp <paul.vernin@grenoble-inp.org>
2022-04-11 19:51:41 +0200

    abr est present tail call

--- tp4/fct_abr_est_present_tail_call.s
+++ tp4/fct_abr_est_present_tail_call.s
@@ -18,20 +18,31 @@ bool abr_est_present_tail_call(uint32_t val, struct noeud_t *abr)
   Fonction :
      abr_est_present_tail_call  : feuille
   Contexte :
-     parametre_0      : registre a0
-     parametre_1      : registre ai; pile *(sp+n)
-     variable_locale0 : registre t0
-     variable_locale1 : pile *(sp+k)
-     ra               : pile *(sp+p)
-     variable_globale : memoire [section nom_de_section]
+     abr : registre a1 (champ de type struct noeud_t*)
+     val : registre a0 (champ de type uint32_t)
  */
 abr_est_present_tail_call:
-    addi sp, sp, -12
-    sw ra, 8(sp)
-    sw a1, 4(sp)
-    sw a0, 0(sp)
 abr_est_present_tail_call_fin_prologue:
+if:
+    bnez a1, elsif1 /* branch si abr != NULL */
+    addi a0, x0, 0 /* return false */
+    j fin_if
+elsif1:
+    lw t1, 0(a1) /*t1 = *abr */
+    bne a0, t1, elsif2 /* branch si val != abr->val */
+    addi a0, x0, 1 /* return true */
+    j fin_if
+elsif2:
+    lw t1, 0(a1)
+    blt a0, t1, else /* branch si val < abr->val*/ 
+    lw a1, 8(a1) /* a1 = abr->fd */
+    j abr_est_present_tail_call
+    /* return abr_est_present_tail_call(val, abr->fg) */
+    j fin_if
+else:
+    lw a1, 4(a1) /* a1 = abr->fg */
+    j abr_est_present_tail_call
+    /* return abr_est_present_tail_call(val, abr->fd) */
+fin_if:
 abr_est_present_tail_call_debut_epilogue:
-    lw ra, 8(sp)
-    addi sp, sp, 12
     ret
